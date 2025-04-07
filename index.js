a2dc4b9bb58c428caf85ca5dd7dccec5ce4d1154
gaetanoquarto <gaetanoqquarto@gmail.com>
2025-04-07 19:25:33 +0200

    cors abilitata al localhost

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(methodOverride('_method'));
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true

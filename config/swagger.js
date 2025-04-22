c19e2d329dd1434f4963f031b0c2b10be0a2da1e
Marco Santoro <91908791+Marcolizzo@users.noreply.github.com>
2025-04-22 20:11:38 +0200

    correzioni doc swagger e aggiunta bundle

--- config/swagger.js
+++ config/swagger.js
@@ -2,7 +2,7 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
 
-const swaggerPath = path.resolve(__dirname, '../docs/swagger.yaml');
+const swaggerPath = path.resolve(__dirname, '../docs/swagger-bundled.yaml');
 const swaggerSpec = YAML.load(swaggerPath);
 
 module.exports = { swaggerUi, swaggerSpec };

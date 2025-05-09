5777737ca7e29cc50d26656559d9c4926a22fdb3
gaetanoquarto <gaetanoqquarto@gmail.com>
2025-05-09 11:13:31 +0200

    aggiunto template reset password

--- constants/emailTemplates.js
+++ constants/emailTemplates.js
@@ -1,6 +1,6 @@
 const EMAIL_TEMPLATES = {
     REGISTRATION: 'd-0fbcb431c5034ecfa0f50e1302380ff7',
-    PASSWORD_RESET: 'd-0fbcb431c5034ecfa0f50e1302380ff7'
+    PASSWORD_RESET: 'd-e4e8d1e0b8284dceb77c5a4b0b982981'
   };
   
   module.exports = EMAIL_TEMPLATES;
